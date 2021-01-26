cffe4ba330ab108b5838eba6fd4a0296f7980850
Tim Macfarlane <timmacfarlane@gmail.com>
2021-01-26 15:21:14 +0100

    turn on shared history so ^R looks at commands from all shells but don't navigate shared history with ^N and ^P

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -1,2 +1,18 @@
-unsetopt sharehistory
-setopt incappendhistory
+setopt sharehistory
+
+# only browse local history with ^N ^P
+function up-line-or-history-local () {
+        zle set-local-history 1
+        zle up-line-or-history
+        zle set-local-history 0
+}
+zle -N up-line-or-history-local
+bindkey "^P" up-line-or-history-local
+
+function down-line-or-history-local () {
+        zle set-local-history 1
+        zle down-line-or-history
+        zle set-local-history 0
+}
+zle -N down-line-or-history-local
+bindkey "^N" down-line-or-history-local
