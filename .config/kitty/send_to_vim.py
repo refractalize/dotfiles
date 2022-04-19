131b2f84833a5c48c05cde76ee99dd48e152af37
Tim Macfarlane <timmacfarlane@gmail.com>
2022-04-19 09:35:42 +0200

    Fix for latest version of kitty

--- .config/kitty/send_to_vim.py
+++ .config/kitty/send_to_vim.py
@@ -1,8 +1,14 @@
-from kitty import keys
-from kitty.fast_data_types import encode_key_for_tty
-from kitty.key_encoding import parse_shortcut, KeyEvent, encode_key_event
+from kitty.key_encoding import (
+    parse_shortcut,
+    KeyEvent,
+    SHIFT,
+    ALT,
+    CTRL,
+    SUPER,
+    HYPER,
+    META
+)
 import re
-from kitty.conf.utils import python_string
 
 def main():
     pass
@@ -10,30 +16,20 @@ def main():
 def is_nvim_process(p):
     return re.search('n?vim', p['cmdline'][0], re.I)
 
-def encode_key_mapping(key_mapping):
+def encode_key_mapping(key_mapping, window):
     mods, key = parse_shortcut(key_mapping)
     event = KeyEvent(
         mods=mods,
         key=key,
-        shift=bool(mods & 1),
-        alt=bool(mods & 2),
-        ctrl=bool(mods & 4),
-        super=bool(mods & 8),
-        hyper=bool(mods & 16),
-        meta=bool(mods & 32),
+        shift=bool(mods & SHIFT),
+        alt=bool(mods & ALT),
+        ctrl=bool(mods & CTRL),
+        super=bool(mods & SUPER),
+        hyper=bool(mods & HYPER),
+        meta=bool(mods & META),
     ).as_window_system_event()
 
-    print(
-        event.key,
-        event.shifted_key,
-        event.alternate_key,
-        event.mods,
-        event.action
-    )
-
-    return encode_key_for_tty(
-        event.key, event.shifted_key, event.alternate_key, event.mods, event.action
-    )
+    return window.encoded_key(event)
 
 def handle_result(args, result, target_window_id, boss):
     window = boss.window_id_map.get(target_window_id)
@@ -44,7 +40,7 @@ def handle_result(args, result, target_window_id, boss):
     has_nvim_foreground_process = next(filter(is_nvim_process, window.child.foreground_processes), None)
 
     if has_nvim_foreground_process and not window.text_for_selection():
-        text = encode_key_mapping(args[2])
+        text = encode_key_mapping(args[2], window)
         window.write_to_child(text)
     elif args[1] == 'copy':
         boss.active_window.copy_to_clipboard()
