e1f492ccde788f9f6e3d854968577bcda949f50c
Tim Macfarlane <timmacfarlane@gmail.com>
2022-05-05 13:34:30 +0200

    better zsh tab titles

--- .zsh/tab-title.zsh
+++ .zsh/tab-title.zsh
@@ -1,3 +1,17 @@
-function tab-title {
-  kitty @ set-tab-title $(basename $(pwd))
+export DISABLE_AUTO_TITLE=true
+
+function set_win_title(){
+  DIR=${PROMPT_ICON:-$PWD:t}
+  BR=$(git branch --show-current 2>/dev/null)
+
+  if [[ -n $BR ]]
+  then
+    TITLE="$DIR ($BR)"
+  else
+    TITLE="$DIR"
+  fi
+
+  echo -ne "\033]0;$TITLE\007"
 }
+
+precmd_functions+=(set_win_title)
