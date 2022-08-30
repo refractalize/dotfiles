573ed5c8048d74ce17f7a8211945ecfb51ac6de4
Tim Macfarlane <timmacfarlane@gmail.com>
2022-08-30 14:21:56 +0200

    use trouble

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -697,6 +697,18 @@ require('packer').startup(function()
 
   use 'folke/tokyonight.nvim'
 
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+
   use({
       "catppuccin/nvim",
       as = "catppuccin"
