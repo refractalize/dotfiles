db6faeee4e74138a4df58a3f088213ca3d448f12
Tim Macfarlane <timmacfarlane@gmail.com>
2022-09-06 13:27:59 +0200

    auto-save

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -130,7 +130,11 @@ source $HOME/.config/nvim/windows.vim
 source $HOME/.config/nvim/ignorelint.vim
 source $HOME/.config/nvim/titlestring.vim
 source $HOME/.config/nvim/quickfix.vim
-lua require('auto-save')
+lua << EOF
+  require('auto-save').setup({
+    ignore_files = { '.config/nvim/lua/plugins.lua' }
+  })
+EOF
 
 autocmd VimResized * wincmd =
 
--- .config/nvim/lua/auto-save.lua
+++ .config/nvim/lua/auto-save.lua
@@ -1,17 +1,48 @@
-vim.api.nvim_create_augroup("AutoSave", {
-  clear = true,
-})
+local defaults = {
+  write_delay = 135,
+  ignore_files = {},
+  ignore_buffer = nil
+}
 
-vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
-    callback = function()
-      local buf = vim.api.nvim_get_current_buf()
+function ignore_buffer(options, buffer)
+  local filename = vim.api.nvim_buf_get_name(buffer)
 
-      vim.defer_fn(function()
-        if vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_get_option(buf, 'modified') then
-          vim.api.nvim_buf_call(buf, function () vim.cmd("silent! lockmarks write") end)
-        end
-      end, 135)
+  local ignored = vim.tbl_filter(
+    function(ignore_path)
+      local pattern = string.sub(vim.fn.glob2regpat(ignore_path), 2)
+      local match = vim.fn.match(filename, pattern)
+      return match >= 0
     end,
-    pattern = "*",
-    group = "AutoSave",
-})
+    options.ignore_files
+  )
+
+  return next(ignored) or (options.ignore_buffer and options.ignore_buffer(buffer))
+end
+
+function setup(options)
+  options = vim.tbl_deep_extend('force', defaults, options or {})
+
+  vim.api.nvim_create_augroup("AutoSave", {
+    clear = true,
+  })
+
+  vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+      callback = function()
+        local buf = vim.api.nvim_get_current_buf()
+
+        if not ignore_buffer(options, buf) then
+          vim.defer_fn(function()
+            if vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_get_option(buf, 'modified') then
+              vim.api.nvim_buf_call(buf, function () vim.cmd("silent! lockmarks write") end)
+            end
+          end, options.write_delay)
+        end
+      end,
+      pattern = "*",
+      group = "AutoSave",
+  })
+end
+
+return {
+  setup = setup
+}
