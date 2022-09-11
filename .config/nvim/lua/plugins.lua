e02e07f2fc674e8886906a8c7f7ab1d35cfaffc1
Tim Macfarlane <timmacfarlane@gmail.com>
2022-09-11 07:26:42 +0200

    hlslens scrollbar

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -612,7 +612,48 @@ require('packer').startup(function()
     end
   }
 
-  use 'google/vim-searchindex'
+  use {
+    'kevinhwang91/nvim-hlslens',
+
+    after = 'nvim-scrollbar',
+
+    config = function()
+      local kopts = {noremap = true, silent = true}
+
+      vim.api.nvim_set_keymap('n', 'n',
+          [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+          kopts)
+      vim.api.nvim_set_keymap('n', 'N',
+          [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+          kopts)
+      vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+      vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+      vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+      vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+
+      vim.api.nvim_set_keymap('n', '<Leader>l', ':noh<CR>', kopts)
+
+      require('hlslens').setup({
+          nearest_only = true,
+      })
+    end
+  }
+
+  use {
+    'petertriho/nvim-scrollbar',
+
+    config = function()
+      require("scrollbar").setup()
+    end
+  }
+
+  use {
+    'rcarriga/nvim-notify',
+
+    config = function()
+      vim.notify = require("notify")
+    end
+  }
 
   use {
     'mfussenegger/nvim-dap',
