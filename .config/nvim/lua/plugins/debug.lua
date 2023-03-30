fc7e5c3a25ffec1bac29ac8753e8685937553654
Tim Macfarlane <timmacfarlane@gmail.com>
2023-03-30 09:06:00 +0200

    debugger ui

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -2,12 +2,12 @@ return {
   {
     "microsoft/vscode-js-debug",
     lazy = true,
-    build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out" 
+    build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
   },
 
   {
     "mxsdev/nvim-dap-vscode-js",
-    dependencies = {"mfussenegger/nvim-dap"},
+    dependencies = { "mfussenegger/nvim-dap" },
 
     config = function()
       require("dap-vscode-js").setup({
@@ -15,7 +15,7 @@ return {
         -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
         debugger_path = "/Users/tim/.local/share/nvim/lazy/vscode-js-debug", -- Path to vscode-js-debug installation.
         -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-        adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+        adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
         -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
         -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
         -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
@@ -38,9 +38,15 @@ return {
             cwd = "${workspaceFolder}",
             console = "integratedTerminal",
             internalConsoleOptions = "neverOpen",
-          }
+          },
         }
       end
-    end
+    end,
+  },
+
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap" },
+    config = true,
   },
 }
