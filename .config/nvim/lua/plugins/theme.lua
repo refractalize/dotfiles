7a6f7db6564a775deccdb7d791a97b25ac2cca73
Tim Macfarlane <timmacfarlane@gmail.com>
2023-04-06 12:03:07 +0200

    everforest

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,116 +1,120 @@
 return {
   {
-    'refractalize/theme',
+    "refractalize/theme",
 
     priotity = 1000,
 
     config = function()
-      vim.cmd[[
+      vim.cmd([[
         call theme#SetupCurrentTheme()
-      ]]
-    end
+      ]])
+    end,
   },
 
   {
-    'tomasr/molokai',
-    lazy = true
+    "tomasr/molokai",
+    lazy = true,
   },
   {
-    'sonph/onehalf',
+    "sonph/onehalf",
 
     lazy = true,
 
     config = function(plugin)
       vim.opt.rtp:append(plugin.dir .. "/vim")
-    end
+    end,
   },
   {
-    'ntk148v/vim-horizon',
+    "ntk148v/vim-horizon",
     lazy = true,
   },
   {
-    'joshdick/onedark.vim',
-    lazy = true
-  },
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
+    "joshdick/onedark.vim",
     lazy = true,
   },
   {
-    'EdenEast/nightfox.nvim',
-    lazy = true
-  },
-  {
-    'morhetz/gruvbox',
-    lazy = true
-  },
-  {
-    'rakr/vim-one',
-    lazy = true
-  },
-  {
-    'glepnir/oceanic-material',
-    lazy = true
-  },
-  {
-    'marko-cerovac/material.nvim',
-    lazy = true
-  },
-  {
-    'rakr/vim-two-firewatch',
-    lazy = true
-  },
-  {
-    'NLKNguyen/papercolor-theme',
-    lazy = true
-  },
-  {
-    'ayu-theme/ayu-vim',
-    lazy = true
-  },
-  {
-    'Lokaltog/vim-monotone',
-    lazy = true
-  },
-  {
-    'yashguptaz/calvera-dark.nvim',
-    lazy = true
-  },
-  {
-    'rebelot/kanagawa.nvim',
-    lazy = true
-  },
-  {
-    'sam4llis/nvim-tundra',
-    lazy = true
-  },
-  {
-    'glepnir/zephyr-nvim',
+    "rose-pine/neovim",
+    name = "rose-pine",
     lazy = true,
   },
   {
-    'overcache/NeoSolarized',
-    lazy = true
+    "EdenEast/nightfox.nvim",
+    lazy = true,
   },
   {
-    'mhartington/oceanic-next',
-    lazy = true
+    "morhetz/gruvbox",
+    lazy = true,
   },
   {
-    'projekt0n/github-nvim-theme',
+    "rakr/vim-one",
+    lazy = true,
+  },
+  {
+    "glepnir/oceanic-material",
+    lazy = true,
+  },
+  {
+    "marko-cerovac/material.nvim",
+    lazy = true,
+  },
+  {
+    "rakr/vim-two-firewatch",
+    lazy = true,
+  },
+  {
+    "NLKNguyen/papercolor-theme",
+    lazy = true,
+  },
+  {
+    "ayu-theme/ayu-vim",
+    lazy = true,
+  },
+  {
+    "Lokaltog/vim-monotone",
+    lazy = true,
+  },
+  {
+    "yashguptaz/calvera-dark.nvim",
+    lazy = true,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+  },
+  {
+    "sam4llis/nvim-tundra",
+    lazy = true,
+  },
+  {
+    "glepnir/zephyr-nvim",
+    lazy = true,
+  },
+  {
+    "overcache/NeoSolarized",
+    lazy = true,
+  },
+  {
+    "mhartington/oceanic-next",
+    lazy = true,
+  },
+  {
+    "projekt0n/github-nvim-theme",
     config = function()
       -- require('github-theme').setup()
     end,
     lazy = true,
   },
   {
-    'folke/tokyonight.nvim',
-    lazy = true
+    "folke/tokyonight.nvim",
+    lazy = true,
   },
   {
     "catppuccin/nvim",
     name = "catppuccin",
     lazy = true,
-  }
+  },
+  {
+    "sainnhe/everforest",
+    lazy = true,
+  },
 }
