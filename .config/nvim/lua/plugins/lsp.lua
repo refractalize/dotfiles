9b1bb19e7c11cada23411fd87a2d09f2b70fdf99
Tim Macfarlane <timmacfarlane@gmail.com>
2024-05-27 12:54:16 +0200

    working omnisharp config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -151,10 +151,47 @@ return {
           },
         },
         omnisharp = {
-          cmd = { "dotnet", os.getenv('HOME') .. "/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll" },
-          enable_editorconfig_support = true,
-          enable_roslyn_analyzers = true,
-          analyze_open_documents_only = true,
+          cmd = { "dotnet", os.getenv("HOME") .. "/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll" },
+
+          settings = {
+            FormattingOptions = {
+              -- Enables support for reading code style, naming convention and analyzer
+              -- settings from .editorconfig.
+              EnableEditorConfigSupport = true,
+              -- Specifies whether 'using' directives should be grouped and sorted during
+              -- document formatting.
+              OrganizeImports = true,
+            },
+            MsBuild = {
+              -- If true, MSBuild project system will only load projects for files that
+              -- were opened in the editor. This setting is useful for big C# codebases
+              -- and allows for faster initialization of code navigation features only
+              -- for projects that are relevant to code that is being edited. With this
+              -- setting enabled OmniSharp may load fewer projects and may thus display
+              -- incomplete reference lists for symbols.
+              LoadProjectsOnDemand = nil,
+            },
+            RoslynExtensionsOptions = {
+              -- Enables support for roslyn analyzers, code fixes and rulesets.
+              enableAnalyzersSupport = true,
+              --
+              -- Enables support for showing unimported types and unimported extension
+              -- methods in completion lists. When committed, the appropriate using
+              -- directive will be added at the top of the current file. This option can
+              -- have a negative impact on initial completion responsiveness,
+              -- particularly for the first few completion sessions after opening a
+              -- solution.
+              enableImportCompletion = nil,
+              -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+              -- true
+              -- AnalyzeOpenDocumentsOnly = nil,
+            },
+            Sdk = {
+              -- Specifies whether to include preview versions of the .NET SDK when
+              -- determining which version to use for project loading.
+              IncludePrereleases = true,
+            },
+          },
           mappings = {
             format = "force",
           },
