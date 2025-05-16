85b339e650ce84a0f6a015b44c358fe2b90ddeb4
Tim Macfarlane <timmacfarlane@gmail.com>
2025-05-16 16:59:05 +0200

    finally ctrl-c ctrl-v copy and paste in the terminal and in neovim

--- src/nvim-plugins/kittycopy/lua/kittycopy.lua
+++ src/nvim-plugins/kittycopy/lua/kittycopy.lua
@@ -1,7 +1,10 @@
 local namespace = vim.api.nvim_create_namespace("kitty-user-var")
 
 -- put this into kitty.conf
--- map --when-focus-on var:nvim cmd+c send_text all \x1by
+-- map --when-focus-on var:nvim ctrl+c no_op
+-- map --when-focus-on var:nvim ctrl+shift+v no_op
+-- map ctrl+c copy_or_interrupt
+-- map ctrl+v paste_from_clipboard
 
 local function setup()
   vim.api.nvim_create_autocmd({ "VimEnter" }, {
@@ -16,8 +19,8 @@ local function setup()
     end,
   })
 
-  vim.keymap.set("n", "<M-y>", '"+y', { noremap = true })
-  vim.keymap.set("v", "<M-y>", '"+y', { noremap = true })
+  vim.keymap.set("v", "<C-c>", '"+y', { noremap = true })
+  vim.keymap.set("n", "<C-S-v>", '<C-v>', { noremap = true })
 end
 
 return {
